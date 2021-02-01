4d0f4a7baef0bb11a4d51a168e2e79b0f4c95bb2
William Chang <wdchang86@gmail.com>
2021-02-01 14:16:20 -0500

    Added npm dotenv and added .env file and process.env variables

---
+++ productSDC/env/config_sample.js
@@ -0,0 +1,6 @@
+module.exports = {
+  host: 'url',
+  user: 'your_username',
+  password: 'your_password',
+  database: 'your_database'
+}
--- productSDC/server.js
+++ productSDC/server.js
@@ -1,19 +1,29 @@
 const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors')
 const db = require('./database/index.js');
 const productRouter = require('./resources/productRouter.js');
+const path = require('path');
+require('dotenv').config({path: path.join(__dirname, '.env')})
 
 const app = express();
 
+app.use(cors({
+  origin: ['http://localhost:3333'],
+  credentials: true
+}));
+
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.get('/', (req, res) => {
-  res.status(200).send('Hello!');
-});
+// app.get('/', (req, res) => {
+//   res.status(200).send('Hello!');
+// });
 
 app.use('/products', productRouter);
 
-PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server listening on Port: ${PORT}`);
 })
