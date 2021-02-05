ad54f7a58f2b0a59434fb2e2ea60271a48b74580
William Chang <wdchang86@gmail.com>
2021-02-05 11:18:07 -0500

    docker-compose works, but issue with querying database

--- productSDC/database/index.js
+++ productSDC/database/index.js
@@ -4,11 +4,20 @@ const path = require('path');
 require('dotenv').config({path: path.join(__dirname, '../env/.env')})
 
 const connection = mysql.createConnection({
-  host: process.env.MYSQL_HOST,
+  host: 'localhost',
   user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
+  password: process.env.MYSQL_ROOT_PASSWORD,
   database: process.env.MYSQL_DATABASE
 });
 const db = bluebird.promisifyAll(connection, {multiArgs: true});
+
+// const connection = mysql.createConnection({
+//   host: process.env.MYSQL_HOST,
+//   port: '3306',
+//   user: process.env.MYSQL_USER,
+//   password: process.env.MYSQL_ROOT_PASSWORD,
+//   database: process.env.MYSQL_DATABASE
+// });
+// const db = bluebird.promisifyAll(connection, {multiArgs: true});
 
 module.exports = db;
